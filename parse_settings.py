38ed28bbe650eb7d1333a636ca7f2607b51537be
Евгений <jamescloudeve@gmail.com>
2022-12-01 20:10:42 +0300

    Initial commit

---
+++ parse_settings.py
@@ -0,0 +1,10 @@
+
+# List of genres. Currently available genres: detectives, fiction, horrors, fantasy, romance, action
+# ['detectives', 'fiction', 'horrors', 'fantasy', 'romance', 'action']
+GENRES = ['horrors']
+
+# Name of a file containing parsing results (books by default)
+OUTPUT_FILE_NAME = 'books'
+
+# Format of a file containing parsing results (json by default)
+OUTPUT_FILE_FORMAT = 'json'
